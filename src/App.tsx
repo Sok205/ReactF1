419abd58c0d9d739b9308e34858ebe00c21f999f
sok205 <Filip007lol@gmail.com>
2025-04-05 14:51:57 +0200

    look overhaul + header addition

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import DriversDisplay from './components/DriversDisplay';
+import AppHeader from './components/AppHeader';
 import { SWRConfig } from 'swr';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         fetcher: (url: string) => fetch(url).then(res => res.json())
       }}
     >
+      <AppHeader />
       <div className="App">
         <main>
           <DriversDisplay />
---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,15 @@
+import '../styles/AppHeader.css';
+
+const AppHeader = () => {
+  return (
+    <header className="app-header">
+      <div className="header-content">
+        <div className="f1-logo">F1</div>
+        <h1>Championship Tracker</h1>
+        <p>2022 Season Standings & Championship Possibilities</p>
+      </div>
+    </header>
+  );
+}
+
+export default AppHeader;
